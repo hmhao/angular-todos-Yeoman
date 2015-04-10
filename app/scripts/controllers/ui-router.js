3a6686fd714ac51ed7d19025822490f6d8a4d55b
hmh <215795583@qq.com>
2015-04-10 21:07:18 +0800

    1、更新注释 2、导航选中效果

--- app/scripts/controllers/ui-router.js
+++ app/scripts/controllers/ui-router.js
@@ -1,29 +1,34 @@
 var uiRouter = angular.module('uiRouter', ['ui.router']);
 uiRouter.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise('/');
     $stateProvider
-        .state('#', {
+        .state('#', {//将template替换到顶层ui-view
             url: '/',
             templateUrl: 'views/main.html',
             controller: 'MainCtrl'
         })
-        .state('search', {
+        .state('search', {//将template替换到顶层ui-view
             url: '/search',
             templateUrl: 'views/search.html',
             controller: 'SearchCtrl'
         })
-        .state('note', {
+        .state('note', {//将template替换到顶层ui-view
             url: '/note',
             templateUrl: 'views/note.html'
         })
-        .state('phones', {
+        .state('phones', {//将template替换到顶层ui-view
             url: '/phones',
             templateUrl: 'views/phone-list.html',
             controller: 'PhoneListCtrl'
         })
+        /*.state('phoneId', {//将template替换到顶层ui-view
+            url: '/phones/:phoneId',
+            templateUrl: 'views/phone-detail.html',
+            controller: 'PhoneDetailCtrl'
+        })*/
         .state('phones.phoneId', {
             url: '/:phoneId',
-            views: {
+            views: {//必须这样写才能将template替换到顶层ui-view
                 '@': {
                     templateUrl: 'views/phone-detail.html',
                     controller: 'PhoneDetailCtrl'
@@ -36,18 +41,18 @@ uiRouter.config(function($stateProvider, $urlRouterProvider) {
                 '': {
                     templateUrl: 'views/ui-router.html'
                 },
-                'topbar@uirouter': {
+                'topbar@uirouter': {//将template替换到自身ui-view=topbar
                     templateUrl: 'views/ui-router/topbar.html'
                 },
-                'main@uirouter': {
+                'main@uirouter': {//将template替换到自身ui-view=main
                     templateUrl: 'views/ui-router/home.html'
                 }
             }
         })
         .state('uirouter.usermng', {
-            url: '/usermng',
+            url: '/usermng',// /uirouter/usermng
             views: {
-                'main': {
+                'main': {//将template替换到uirouter中ui-view=main
                     templateUrl: 'views/ui-router/usermng.html',
                     controller: function($scope, $state) {
                         $scope.addUserType = function() {
@@ -57,16 +62,16 @@ uiRouter.config(function($stateProvider, $urlRouterProvider) {
                 }
             }
         })
-        .state('uirouter.usermng.highendusers', {
-            url: '/highendusers',
+        .state('uirouter.usermng.highendusers', {//将template替换到uirouter.usermng中ui-view
+            url: '/highendusers',// /uirouter/usermng/highendusers
             templateUrl: 'views/ui-router/highendusers.html'
         })
-        .state('uirouter.usermng.normalusers', {
-            url: '/normalusers',
+        .state('uirouter.usermng.normalusers', {//将template替换到uirouter.usermng中ui-view
+            url: '/normalusers',// /uirouter/usermng/normalusers
             templateUrl: 'views/ui-router/normalusers.html'
         })
-        .state('uirouter.usermng.lowusers', {
-            url: '/lowusers',
+        .state('uirouter.usermng.lowusers', {//将template替换到uirouter.usermng中ui-view
+            url: '/lowusers',// /uirouter/usermng/lowusers
             templateUrl: 'views/ui-router/lowusers.html'
         })
         .state('uirouter.usermng.addusertype', {
@@ -79,25 +84,25 @@ uiRouter.config(function($stateProvider, $urlRouterProvider) {
             }
         })
         .state('uirouter.permission', {
-            url: '/permission',
+            url: '/permission',// /uirouter/permission
             views: {
-                'main': {
+                'main': {//将template替换到uirouter中ui-view=main
                     template: '这里是权限管理'
                 }
             }
         })
         .state('uirouter.report', {
-            url: '/report',
+            url: '/report',// /uirouter/report
             views: {
-                'main': {
+                'main': {//将template替换到uirouter中ui-view=main
                     template: '这里是报表管理'
                 }
             }
         })
         .state('uirouter.settings', {
-            url: '/settings',
+            url: '/settings',// /uirouter/settings
             views: {
-                'main': {
+                'main': {//将template替换到uirouter中ui-view=main
                     template: '这里是系统设置'
                 }
             }
