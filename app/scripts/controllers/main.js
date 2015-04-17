83da3782946fbedadf404c7d2e2fa8182d0bca08
hmh <215795583@qq.com>
2015-04-17 16:39:54 +0800

    实现一个手风琴指令

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,4 +21,63 @@ angular.module('nodejsYoApp')
           $scope.removeTodo = function (index) {
               $scope.todos.splice(index, 1);
           };
+    }).directive("todoAccordion", function () {
+        return {
+          restrict: "E",
+          transclude: true,//在指令模板中声明ng-transclude的元素内嵌入
+          replace: true,//替换页面<todo-accordion>
+          scope: {},
+          template:"<div class='panel-group' ng-transclude></div>",
+          link: function (scope, element, attrs) {
+
+            // 确保 accordion拥有id
+            var id = element.attr("id");
+            if (!id) {
+              id = "todo-accordion-" + scope.$id;
+              element.attr("id", id);
+            }
+
+            // 在 accordion-toggle 元素设置 data-parent 和 href 属性
+            var arr = element.find(".panel-title");
+            for (var i = 0; i < arr.length; i++) {
+              $(arr[i]).attr("data-parent", "#" + id);
+              $(arr[i]).attr("href", "#" + id + "-collapse-" + i);
+            }
+
+            // 在 accordion-body 元素设置 collapse 属性
+            // 并展开第一个面板
+            arr = element.find(".panel-collapse");
+            $(arr[0]).addClass("in"); // 展开第一个面板
+            for (var i = 0; i < arr.length; i++) {
+              $(arr[i]).attr("id", id + "-collapse-" + i);
+            }
+          },
+          controller: function () {}
+        };
+    }).directive('todoAccordionPane', function () {
+        return {
+          require: "^todoAccordion",
+          restrict: "E",
+          transclude: true,//在指令模板中声明ng-transclude的元素内嵌入
+          replace: true,//替换<todo-accordion-pane>
+          scope: {
+            title: "@"
+          },
+          template:
+            "<div class='panel panel-default'>" +
+            "  <div class='panel-heading'>" +
+            "    <a class='panel-title collapsed' data-toggle='collapse'>{{title}}</a>" +
+            "  </div>" +
+            "<div class='panel-collapse collapse'>" +
+            "  <div class='panel-body' ng-transclude></div>" +
+            "  </div>" +
+            "</div>",
+          link: function (scope, element, attrs) {
+            scope.$watch("title", function () {
+              // NOTE: this requires jQuery (jQLite won't do html)
+              var hdr = element.find(".panel-title");
+              hdr.html(scope.title);
+            });
+          }
+        };
     });
